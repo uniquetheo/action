a6c84be227ff35670cdd6f771b7da86117ebe001
theophilus-mercifield <128309455+theophilus-mercifield@users.noreply.github.com>
2023-11-10 13:37:42 +0000

    Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Signup } from './Components/Signup'
-import { Login } from './Components/Login'
+import { Signup } from './Components/Pages/Signup'
+import { Login } from './Components/Pages/Login'
+import { Register } from './Components/Pages/Register'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />}/>
         <Route path='/signup' element={<Signup />}/>
+        <Route path='/register' element={<Register />}/>
       </Routes>
     </Router>
   )
