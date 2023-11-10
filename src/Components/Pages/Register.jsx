b6bc69ace06db310dd516799040321378ba0e6df
theophilus-mercifield <128309455+theophilus-mercifield@users.noreply.github.com>
2023-11-10 13:37:42 +0000

    Register

---
+++ src/Components/Pages/Register.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Register = () => {
+  return (
+    <Main>
+        <h1>Register</h1>
+    </Main>
+  )
+}
+
+const Main = styled.main``
--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Form } from "./Form/Form.styles";
+import { Form } from "../Form/Form.styles";
 
 export const Signup = () => {
   return (
