6b394f34c3fb50265fa38d0d81ade81646cdda35
theophilus-mercifield <128309455+theophilus-mercifield@users.noreply.github.com>
2023-11-13 10:26:05 +0000

    edit: directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Signup } from './Components/Signup'
-import { Login } from './Components/Login'
-import {Router} from "./Router.jsx";
+import { Signup } from './Components/Pages/Signup'
+import { Login } from './Components/Pages/Login'
+import { Register } from './Components/Pages/Register'
 
 
 function App() {
 
   return (
-      <Router />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/register' element={<Register />}/>
+      </Routes>
+    </Router>
   )
 }
 
