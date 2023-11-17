a65afc643c6e99bf28ece105b7c5dae5569cd4ee
theophilus-mercifield <128309455+theophilus-mercifield@users.noreply.github.com>
2023-11-17 18:41:30 +0000

    update student register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,40 +1,105 @@
 /* eslint-disable react/no-unescaped-entities */
 import styled from "styled-components";
-import { StyledForm, StyledFormWrapper } from "../Components/Form/Form.styles.jsx";
+import {
+  StyledForm,
+  StyledFormWrapper,
+} from "../Components/Form/Form.styles.jsx";
 import { StyledButton } from "../Components/Button/Button.styles.jsx";
+import logo from "../assets/nic_logo_transparent.png";
+import Input from "../Components/commons/Input.jsx";
+// import { validateFields } from "../utils/helpers.js";
 
 export const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const labels = ["Full Name", "Age"];
+
+  const levels = ["Select Class","Primary4", "Primary5", "Primary6", "JHS1", "JHS2", "JHS3"];
+
+  const optionNumbers = ["Select Option","Number1", "Number2", "Number3"];
+
   return (
     <Main>
       <StyledFormWrapper>
-        <StyledForm1>
+        <StyledForm1 onSubmit={handleSubmit}>
           <Logo>
-            <img src="/assets/tp_nic_logo.png" alt="logo" />
+            <img src={logo} alt="logo" />
           </Logo>
           <h2>Register a student for the Quiz</h2>
           <span>Add student's details below</span>
-          <label htmlFor="fullname">Full Name</label>
+
+          {/* {
+            labels.map((label) => {
+              return (
+                <input key={label}
+                name={label.toLowerCase()}
+                label={label}
+                // data={data}
+                // setData={setData}
+                // isError={isError}
+                // setIsError={setIsError}
+                type={label.toLowerCase().includes('number') ? 'number' : 'text'} />
+              )
+            })} */}
+
+          {labels.map((label) => {
+            return (
+              <>
+                <label htmlFor={label.toLowerCase()}>{label}</label>
+                <input
+                  name={label.toLowerCase()}
+                  type={label.toLowerCase().includes("age") ? "number" : "text"}
+                />
+              </>
+            );
+          })}
+          <label htmlFor="Class">Class</label>
+          <select name="Class" id="Class">
+            {levels.map((level) => {
+              return (
+                <>
+                  {/* <label htmlFor="level">Class</label> */}
+                  <option value={level.toLowerCase()}>{level}</option>
+                </>
+              );
+            })}
+          </select>
+
+          <label htmlFor="Option">Option</label>
+          <select name="option" id="option">
+            {
+              optionNumbers.map((optionNumber)=>{
+                return (
+                <option value={optionNumber.toLowerCase()}>{optionNumber}</option>
+                )
+              })
+            }
+          </select>
+
+          {/* <label htmlFor="fullname">Full Name</label>
           <input type="text" id="fullname" name="fullname" />
           <label htmlFor="age">Age</label>
           <input type="number" id="age" name="age" />
-          <label htmlFor="Class">Class</label>
-          <select name="Class" id="Class">
+          <label htmlFor="Class">Class</label> */}
+          {/* <select name="Class" id="Class">
             <option value="">Select Class</option>
-            <option value="Basic4">Basic4</option>
-            <option value="Basic5">Basic5</option>
-            <option value="Basic6">Basic6</option>
+            <option value="Primary4">Primary4</option>
+            <option value="Primary5">Primary5</option>
+            <option value="Primary6">Primary6</option>
             <option value="JHS1">JHS1</option>
             <option value="JHS2">JHS2</option>
             <option value="JHS3">JHS3</option>
           </select>
-          <label htmlFor="Number">Number</label>
+          {/* <label htmlFor="Number">Number</label> 
           <select name="number" id="number">
             <option value="">Select Number</option>
             <option value="1">Number 1</option>
             <option value="2">Number 2</option>
             <option value="3">Number 3</option>
-          </select>
-          <StyledButton>Register</StyledButton>
+          </select> */}
+          <StyledButton type="submit">Register</StyledButton>
         </StyledForm1>
       </StyledFormWrapper>
     </Main>
@@ -48,6 +113,7 @@ const Main = styled.main`
 `;
 
 const StyledForm1 = styled(StyledForm)`
+  align-items: center;
   h2 {
     font-size: 24px;
     font-weight: 600;
